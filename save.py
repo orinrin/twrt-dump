a822694ee53a865d641a52523d61f26fc05974e6
おりんりん <nyaa@orinr.in>
2017-04-17 16:56:45 -0700

    added function to parse likes

--- save.py
+++ save.py
@@ -8,8 +8,17 @@ import requests
 listfile = "save.csv"
 imagedir = "save.out"
 
+username = "YourHandle"
+likefile = "likesave.csv"
+
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+
 try: sys.argv[1]
 except: sys.argv.append(0)
+
+if sys.argv[1] == "ldl":
+	sys.argv[1] = "dl"
+	listfile = likefile
 
 if sys.argv[1] == "mk":
 	with open(listfile, "wb") as list:
@@ -34,6 +43,7 @@ if sys.argv[1] == "mk":
 						if url.find("/media/") < 0: continue
 						
 						list.write("{},{},{}\r\n".format(user, exp, url))
+
 elif sys.argv[1] == "dl":
 	if os.path.isfile(listfile):
 		if not os.path.isdir(imagedir): os.mkdir(imagedir)
@@ -42,8 +52,6 @@ elif sys.argv[1] == "dl":
 			urls = list.read().split("\r\n")[1:]
 			
 			with requests.Session() as session:
-				headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
-				
 				for url in urls:
 					url = url.strip().split(",")[-1]
 					dest = imagedir + os.sep + url.split("/")[-1]
@@ -54,9 +62,82 @@ elif sys.argv[1] == "dl":
 					print url
 					
 					with open(dest, "wb") as handle:
-						response = session.get(url + ":orig", headers = headers, stream = True)
-						time.sleep(8)
+						fail = 0
+						
+						while fail < 3:
+							try:
+								response = session.get(url + ":orig", headers = headers, stream = True)
+								time.sleep(8)
+								break
+							except:
+								fail += 1
+								time.sleep(16)
+						
+						if fail > 2: print "unable to retrieve image"; quit()
 						
 						for block in response.iter_content(1024): handle.write(block)
+	
 	else: print "no list found"
+
+elif sys.argv[1] == "lmk":
+	import urllib
+	
+	from bs4 import BeautifulSoup
+	
+	with open(likefile, "wb") as list:
+		list.write("User,TweetURL,MediaURL\r\n")
+		
+		with requests.Session() as session:
+			ids = []
+			last = -1
+			
+			while last != 0:
+				# bypass the developer api entirely using widget calls, likely to break in the future
+				if last < 0: request = "https://cdn.syndication.twimg.com/timeline/likes?callback=__twttr.callbacks.tl_i1_likes_{0}_old&lang=en&screen_name={0}&suppress_response_codes=true".format(username)
+				else: request = "https://cdn.syndication.twimg.com/timeline/likes?callback=__twttr.callbacks.tl_i1_likes_{0}_old&lang=en&max_position={1}&screen_name={0}&suppress_response_codes=true".format(username, last)
+				
+				print request
+				
+				last = 0
+				fail = 0
+				
+				while fail < 3:
+					try:
+						response = session.get(request, headers = headers)
+						time.sleep(8)
+						break
+					except:
+						fail += 1
+						time.sleep(16)
+				
+				if fail > 2: print "unable to retrieve timeline"; quit()
+				
+				# no idea what response is sent when i reach an invalid timeline, but im pretty sure its not going to have an html body lol
+				try: soup = BeautifulSoup(json.loads(response.text.split("(", 1)[-1].rsplit(")", 1)[0])["body"], "html.parser")
+				except: break
+				
+				tweets = soup.findAll("div", {"data-scribe" : "component:tweet"})
+				
+				if len(tweets) < 1: break
+				
+				for tweet in tweets:
+					last = tweet["data-tweet-id"]
+					
+					if last in ids: last = 0; break
+					
+					ids.append(last)
+					
+					user = tweet.find("span", {"data-scribe" : "element:screen_name"})["title"]
+					
+					# STUPID WORKAROUND FOR TWEETS WITH MULTIPLE IMAGES THAT RELIES ON A LOCALE SPECIFIC STRING
+					pics = tweet.findAll("img", {"title" : "View image on Twitter"})
+					
+					for pic in pics:
+						exp = pic.parent["href"]
+						
+						try: url = urllib.unquote(pic["data-srcset"]).split(" ")[0].split(":small")[0].split(":large")[0].split(":orig")[0]
+						except: continue
+						
+						list.write("{},{},{}\r\n".format(user, exp, url))
+
 else: print "invalid arg"
